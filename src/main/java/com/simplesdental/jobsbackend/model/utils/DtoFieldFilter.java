21a5a1e39463c86a9689beda54203318e4f72ec8
ALEXANDRE LIMA <alexandre.lima0451@gmail.com>
2023-04-03 18:05:43 -0300

    Remover conversao de List para Map, ao retornar todos os campos em Map<String, Map>

--- src/main/java/com/simplesdental/jobsbackend/model/utils/DtoFieldFilter.java
+++ src/main/java/com/simplesdental/jobsbackend/model/utils/DtoFieldFilter.java
@@ -5,15 +5,15 @@ import com.simplesdental.jobsbackend.model.enums.CollectionInitializerEnum;
 
 import java.lang.reflect.Field;
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import static java.util.function.Function.identity;
 
 public abstract class DtoFieldFilter {
 
-    private List<Field> getAllFields(List<Field> fields, Class<?> type) {
-        fields.addAll(Arrays.asList(type.getDeclaredFields()));
+    private Map<String, Field> getAllFields(Map<String, Field> fields, Class<?> type) {
+
+        var declaredFields = type.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            fields.put(declaredField.getName(), declaredField);
+        }
 
         if (type.getSuperclass() != null) {
             getAllFields(fields, type.getSuperclass());
@@ -23,16 +23,14 @@ public abstract class DtoFieldFilter {
     }
 
     private void setNullOrEmpty(Field field) throws IllegalAccessException {
-        CollectionInitializerEnum initializer = CollectionInitializerEnum.fromString(field.getType().getName());
+        var initializer = CollectionInitializerEnum.fromString(field.getType().getName());
         field.setAccessible(true);
         field.set(this, initializer.getEmptyCollection());
     }
 
     public void filterByFields(QueryFilterDto dto) throws IllegalAccessException {
-        List<Field> fields = new ArrayList<>();
-        Map<String, Field> allFields = getAllFields(fields, this.getClass())
-                .stream()
-                .collect(Collectors.toMap(Field::getName, identity()));
+        Map<String, Field> allFields = new HashMap<>();
+        getAllFields(allFields, this.getClass());
 
         dto.getFields().forEach(allFields::remove);
         for (Field field : allFields.values()) {
